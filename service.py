90f9412f841f4d3abdccf6f0530cddc2587039d3
Jacob <jacobevory@gmail.com>
2017-12-08 15:08:52 -0500

    Integrated csv reader and dollar calculation

--- service.py
+++ service.py
@@ -21,7 +21,7 @@ import requests
 import csv
 import pprint
 
-stockGainTot = 0
+totalDollarChange = 0
 
 reader = csv.reader(open('stockinput.csv', 'r'))
 symbols = {}
@@ -31,8 +31,8 @@ for line in reader:
 
 pprint.pprint(symbols)
 
-for stock, boughtPrice in symbols.items():
-    r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stock + '&apikey=' + key)
+for stockSymbol, stockQuantity in symbols.items():
+    r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stockSymbol + '&apikey=' + AVKEY)
     d = r.json()
     series = d['Time Series (Daily)']
     timeData = next(iter(series))
@@ -43,7 +43,56 @@ for stock, boughtPrice in symbols.items():
     openVal = stockData['1. open']
 
     stockGain = (float(closeVal) - float(openVal))
-    stockGainTot = stockGainTot + stockGain
+    totalDollarChange = totalDollarChange + stockGain
     print('The close value for ' + stock + ' on '+ timeData + ' is ' + closeVal + ' (' + str(stockGain) + ')')
 
-print('Overall stock gain: %.2f' % stockGainTot)
+print('Overall stock gain: %.2f' % totalDollarChange)#!/usr/bin/env python3
+
+from avkey.py import AVKEY
+import json
+import requests
+import csv
+
+#imported 2 dimensional wtf ever: inputdata
+
+#Checkpoint: Initialize appropriate libraries, time, json, etc.
+stockSymbol = 0
+stockRequestURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" stockSymbol "&outputsize=compact&apikey=" AVKEY
+#Checkpoint: Read stock data from file and store in 2 dimensional array/vector/mapped-list wtf ever
+reader = csv.reader(open('stockinput.csv', 'r'))
+symbols = {}
+for line in reader:
+    k, v = line
+    symbols[k] = v
+
+pprint.pprint(symbols)
+#Checkpoint: Enter infinite loop
+while true:
+    #Checkpoint: Reset global change variable
+    totalDollarChange = 0
+    totalPercentageChange = 0
+    #Checkpoint: Make http request for each stock, store first set of data in array
+    #Checkpoint: Check close price against open price, calculate $$$ difference, add to global day change variable
+    #Checkpoint: Repeat for all stocks in list
+    for stockSymbol, stockQuantity in symbols.items():
+        d = (requests.get(stockRequestURL)).json()
+        series = d['Time Series (Daily)']
+        timeData = next(iter(series))
+
+        #get most recent close value
+        stockData = series[timeData]
+        closeVal = stockData['4. close']
+        openVal = stockData['1. open']
+
+        stockGain = (float(closeVal) - float(openVal))
+        totalDollarChange = totalDollarChange + stockGain
+        print('The close value for ' + stock + ' on '+ timeData + ' is ' + closeVal + ' (' + str(stockGain) + ')')
+		
+#Checkpoint: Calculate PERCENTAGE change overall
+
+#Checkpoint: Compare and store percentage change to pre-defined intervals
+#Checkpoint: Set LED color to reflect percentage change
+#                Red:     change < -4%
+#                Yellow:    -4% > change > 4%
+#                Green:    4% > change
+#            Potentially define gradient, the RGB ring is capable of anything.
