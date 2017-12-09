ad559aef21d9bc28fb76b00cd0cc364d5c88a056
jacobevory <jacobevory@gmail.com>
2017-12-08 20:05:17 -0500

    Need colorWipe function defined

--- service.py
+++ service.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 #Checkpoint: Initialize appropriate libraries, time, json, etc.
@@ -8,16 +9,64 @@ import csv
 import smtplib
 import pprint
 
-def setLED():
-   #do work
-   return
+from neopixel import *
+import argparse
+import signal
+import sys
 
+def colorWipe():
+	
+
+def signal_handler(signal, frame):
+		colorWipe(strip, Color(0,0,0))
+		sys.exit(0)
+
+def opt_parse():
+		parser = argparse.ArgumentParser()
+		parser.add_argument('-c', action='store_true', help='clear the display on exit')
+		args = parser.parse_args()
+		if args.c:
+				signal.signal(signal.SIGINT, signal_handler)
+
+totalPercentageChange = 0
 upperLimit = 4
 lowerLimit = 4
 
+# LED strip configuration:
+LED_COUNT	  = 12	  # Number of LED pixels.
+LED_PIN		= 18	  # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN		= 10	  # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ	= 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA		= 5	   # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255	 # Set to 0 for darkest and 255 for brightest
+LED_INVERT	 = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL	= 0	   # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP	  = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+def setLED():
+	if totalPercentageChange >= upperLimit :
+		 for i in range(strip.numPixels()):
+                        strip.setPixelColor(i, Color(0, 150, 0))
+                        strip.show()
+	elif totalPercentageChange <= -1*lowerLimit :
+		 for i in range(strip.numPixels()):
+                        strip.setPixelColor(i, Color(150, 0, 0))
+                        strip.show()
+	else :
+		 for i in range(strip.numPixels()):
+                        strip.setPixelColor(i, Color(0, 0, 150))
+                        strip.show()
+	return
+
+setLED()
 SMSgreeting = 'Hello! Thank you for signing up for the Peaceful Portfolio Notification System. You will be notified if your portfolio changes more than 4%.'
-SMSlower = 'Your portfolio has decreased by more than ' + lowerLimit + '%.'
-SMSupper = 'Your portfolio has increased by more than ' + upperLimit + '%.'
+SMSlower = 'Your portfolio has decreased by more than ' + str(lowerLimit) + '%.'
+SMSupper = 'Your portfolio has increased by more than ' + str(upperLimit) + '%.'
 SMSsendFrom = 'peacefulportfolio'
 SMSaddress = '7576331950@vtext.com'
 seriesType = 'Time Series (1min)'
@@ -42,7 +91,8 @@ for line in reader:
 pprint.pprint(symbols)
 #Checkpoint: Enter infinite loop
 k = 1
-while k == 1:
+while True:
+	opt_parse()
 	k+=1
 	#Checkpoint: Reset global change variable
 	totalDollarChange = 0
